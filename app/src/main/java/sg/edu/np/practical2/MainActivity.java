24bf1c7a994a61c34a202a51f9cf5cbdb201bbf3
Shi Jie Soo <s10204933@connect.np.edu.sg>
2021-04-28 17:07:50 +0800

    Practical 2

---
+++ app/src/main/java/sg/edu/np/practical2/MainActivity.java
@@ -0,0 +1,46 @@
+package sg.edu.np.practical2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        User user = new User("MAD",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
+                        " sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
+
+        TextView nameView = findViewById(R.id.textView2);
+        nameView.setText(user.getName());
+        TextView descView = findViewById(R.id.textView);
+        descView.setText(user.getDescription());
+
+        Button followButton = findViewById(R.id.followButton);
+        updateFollowButton(user.isFollowed(), followButton);
+
+
+        followButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user.setFollowed(!user.isFollowed());
+                updateFollowButton(user.isFollowed(), followButton);
+            }
+        });
+    }
+
+    protected void updateFollowButton(boolean isFollowed, Button followButton) {
+        if (isFollowed == true) {
+            followButton.setText("unfollow");
+        } else {
+            followButton.setText("follow");
+        }
+    }
+}
