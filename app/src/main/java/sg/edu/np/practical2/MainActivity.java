1b55fe44a36cc3a8f681dd20ae06f2f0d2c48e0b
Shi Jie Soo <s10204933@connect.np.edu.sg>
2021-05-07 09:28:04 +0800

    Practical 2- figuring out what XMLtest doesnt like

--- app/src/main/java/sg/edu/np/practical2/MainActivity.java
+++ app/src/main/java/sg/edu/np/practical2/MainActivity.java
@@ -2,10 +2,12 @@ package sg.edu.np.practical2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,18 +16,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Intent intent = getIntent();
+        int randint = intent.getIntExtra("randint", 0);
+
         User user = new User("MAD",
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
                         " sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
 
         TextView nameView = findViewById(R.id.textView2);
-        nameView.setText(user.getName());
+        nameView.setText(user.getName() + ' ' + randint);
         TextView descView = findViewById(R.id.textView);
         descView.setText(user.getDescription());
 
         Button followButton = findViewById(R.id.followButton);
-        updateFollowButton(user.isFollowed(), followButton);
-
 
         followButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,12 +39,15 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    protected void updateFollowButton(boolean isFollowed, Button followButton) {
+    private void updateFollowButton(boolean isFollowed, Button followButton) {
+        String toast = "Followed";
         if (isFollowed == true) {
             followButton.setText("unfollow");
         } else {
+            toast = "Unfollowed";
             followButton.setText("follow");
         }
+        Toast.makeText(getApplicationContext(), toast, Toast.LENGTH_SHORT).show();
     }
 
 }
